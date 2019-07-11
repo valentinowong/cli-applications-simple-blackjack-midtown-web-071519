9ccd6bbb1c2e0c1384f22ccf98d8586f13e2f29c
Valentino Wong <valentino.wong@gmail.com>
2019-07-11 18:30:12 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,7 +52,7 @@ end
 
 def runner
   welcome
-  current_card_total = initial_round
+  initial_round
   until current_card_total > 21
     display_card_total(current_card_total)
     current_card_total = hit?(current_card_total)
